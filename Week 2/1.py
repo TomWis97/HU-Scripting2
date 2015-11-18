791bf30bf336d515819c2df3d9d5c0584f3d88f6
TomWis97 <github@tomwishaupt.nl>
2015-11-18 10:32:16 +0100

    Fixed delimiter

--- Week 2/1.py
+++ Week 2/1.py
@@ -1,10 +1,8 @@
 import csv
-csv.register_dialect('puntcomma', delimiter=';')
 f = open('klanten.csv', 'rt')
 try:
-    reader = csv.reader(f, dialect='puntcomma')
+    reader = csv.reader(f)
     for row in reader:
         print(row[0], row[2])
 finally:
     f.close()
-
